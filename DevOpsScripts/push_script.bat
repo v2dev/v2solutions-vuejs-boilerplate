0f962ea02ea889ee11f9c97e276c7891be279461
Rajesh Suvarna <rajesh.suvarna@v2solutions.com>
2024-01-22 15:07:08 +0530

    image tag updated

--- DevOpsScripts/push_script.bat
+++ DevOpsScripts/push_script.bat
@@ -2,5 +2,5 @@
 
 set build_number=%1
 
-docker tag nodejs:%build_number% v2devops/v2solutions-vuejs-boilerplate:%build_number%
+docker tag vuejs:%build_number% v2devops/v2solutions-vuejs-boilerplate:%build_number%
 docker push v2devops/v2solutions-vuejs-boilerplate:%build_number%
